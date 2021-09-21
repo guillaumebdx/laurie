71a6c6f016dbe26f5c8694c25da7177b89eeca51
guillaumebdx <guillaumeharari@hotmail.com>
2021-09-21 23:07:46 +0200

    home & bio done for v1

---
+++ src/Controller/BioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BioController extends AbstractController
+{
+    /**
+     * @Route("/bio", name="bio")
+     */
+    public function index(): Response
+    {
+        return $this->render('bio/index.html.twig', [
+            'controller_name' => 'BioController',
+        ]);
+    }
+}
